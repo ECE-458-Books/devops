57da2c322b5621f3d25f11c9f00b6e03c6ce47ff
hkder <samhskim1@gmail.com>
2023-02-06 17:18:15 -0500

    delete scripts

--- scripts/setup_task.sh
+++
@@ -1,6 +0,0 @@
-# Install snap
-sudo apt update
-sudo apt install snapd -y
-
-# Install task using snap
-sudo snap install task --classic
---
+++ setup_scripts/setup_frontend_dev_env.sh
@@ -0,0 +1,7 @@
+# get NVM (Node Version Manager)
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+# Use NVM most lts (v18.14.0 at 2/5/2023)
+nvm install --lts
+
+# 
