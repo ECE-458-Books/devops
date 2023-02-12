c6962ea8e1cbec7659d4214da1652eccb2ccdd8b
Hosung Kim <hk196@duke.edu>
2023-02-12 18:21:58 -0500

    added system_setup for jenkins

---
+++ jenkins/jenkins_setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# ----------------------------------------------------------------------
+# Copyright © 2023 Hosung Kim <hk196@duke.edu>
+#
+# All rights reserved
+# ----------------------------------------------------------------------
+
+# Install Java
+sudo apt update
+sudo apt install openjdk-11-jre -y
+java -version
+
+# Install Jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
--- system_setup.sh
+++ system_setup.sh
@@ -6,45 +6,102 @@
 # All rights reserved
 # ----------------------------------------------------------------------
 
-BASEDIR=$(pwd)
+# ---------------------------------------------------------------------
+# Functions
+# ---------------------------------------------------------------------
+Setup(){
+    BASEDIR=$(pwd)
 
-# Install Docker
-if [ -x "$(command -v docker)" ]; then
-    echo "Docker installed at $(command -v docker)"
-else
-    echo "Install docker"
-    # command
-    ./setup_scripts/setup_docker.sh
-fi
+    # Install Docker
+    if [ -x "$(command -v docker)" ]; then
+        echo "Docker installed at $(command -v docker)"
+    else
+        echo "Install docker"
+        # command
+        ./setup_scripts/setup_docker.sh
+    fi
 
-# Pre-setup for nginx webserver
-# Step 1. Check for hostname
-echo "Please input hostname.."
-read SERVER_NAME
+    # Pre-setup for nginx webserver
+    # Step 1. Check for hostname
+    echo "Please input hostname.."
+    read SERVER_NAME
 
-export SERVER_NAME=$SERVER_NAME
-source ~/.bashrc
+    export SERVER_NAME=$SERVER_NAME
+    source ~/.bashrc
 
-# Step 2. Set env variable for server_name and create conf file for nginx
-cd $BASEDIR/nginx-https/nginx/setup_conf
-envsubst < ./default.conf.template > ./default.conf
+    # Step 2. Set env variable for server_name and create conf file for nginx
+    cd $BASEDIR/nginx-https/nginx/setup_conf
+    envsubst < ./default.conf.template > ./default.conf
 
-# Start nginx webserver for certificates
-echo "Starting nginx server for SSL certificate using Let's Encrypt"
-cd $BASEDIR/nginx-https
-docker compose up -d webserver_setup
+    # Start nginx webserver for certificates
+    echo "Starting nginx server for SSL certificate using Let's Encrypt"
+    cd $BASEDIR/nginx-https
+    docker compose up -d webserver_setup
 
-# Start Certbot for HTTPS
-docker compose run --rm certbot_setup certonly --webroot --webroot-path /var/www/certbot/ -d $SERVER_NAME
+    # Start Certbot for HTTPS
+    docker compose run --rm certbot_setup certonly --webroot --webroot-path /var/www/certbot/ -d $SERVER_NAME
 
-# After success delete the default.conf file
-rm -rf $BASEDIR/nginx-https/nginx/setup_conf/default.conf
+    # After success delete the default.conf file
+    rm -rf $BASEDIR/nginx-https/nginx/setup_conf/default.conf
 
-# Delete All running Docker Container
-docker compose down
+    # Delete All running Docker Container
+    docker compose down
+}
 
-# Start nginx
-cd $BASEDIR/nginx-https/nginx/conf
-envsubst '${SERVER_NAME}' < ./default.conf.template > ./default.conf
-docker compose up -d webserver_deployment
-docker compose up -d certbot_deployment
+Django(){
+    # Go to conf files
+    cd $BASEDIR/nginx-https/nginx/conf
+    envsubst '${SERVER_NAME}' < ./default.conf.template > ./default.conf
+    docker compose up -d webserver_deployment
+    docker compose up -d certbot_deployment
+}
+
+Jenkins(){
+    # Go to conf files
+    cd $BASEDIR/nginx-https/nginx/conf
+    envsubst '${SERVER_NAME}' < ./jenkins.conf.template > ./default.conf
+    docker compose up -d webserver_deployment
+    docker compose up -d certbot_deployment
+}
+
+Help()
+{
+   # Display Help
+   echo "Script to add Setup System for Django/Jenkins/PostgreSQL"
+   echo
+   echo "Syntax: ./system_setup.sh [-h] [-t [django|jenkins|postgres]"
+   echo "options:"
+   echo "h     Print this Help."
+   echo "t     Select type of system setup [django|jenkins|postgres]"
+   echo
+}
+
+# ---------------------------------------------------------------------
+# Main body of script starts here
+# ---------------------------------------------------------------------
+while getopts ":ht:" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      t) # Enter Users
+        userargs=${OPTARG}
+        if [ $userargs == "django" ]; then
+            Setup
+            Django
+        elif [ $userargs == "jenkins" ]; then
+            Setup
+            Jenkins
+        elif [ $userargs == "postgres" ]; then
+            echo "${userargs} system setup not supported yet"
+        else
+            Help
+        fi
+	 exit;;
+      \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+Help
